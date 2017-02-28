f5691828e00eb5c84dd614a6ae486ed998604c26
Carlos Ribeiro <mail@carlosribeiro.me>
2017-02-28 14:47:09 -0300

    Helper to add a error description field near it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,13 @@ module ApplicationHelper
     }[flash_type.to_sym] || flash_type.to_s
   end
 
+  def error_tag(model, attribute)
+    if model.errors.has_key? attribute
+      content_tag(
+        :div,
+        model.errors[attribute].join(', '),
+        class: 'error_message'
+      )
+    end
+  end
 end
