d9bf73cc88c955b890ea2367b31ce4da77354544
Carlos Ribeiro <mail@carlosribeiro.me>
2017-02-27 21:50:08 -0300

    Tests for create a new vacancy

--- spec/features/register_vacancy_spec.rb
+++ spec/features/register_vacancy_spec.rb
@@ -7,13 +7,15 @@ RSpec.feature 'Register Vacancy' do
 
       fill_in 'Job title',   with: 'Stormtrooper'
       fill_in 'Location',    with: 'Death Star'
-      fill_in 'Description', with: 'You will handle the security of the Death Star. You do not need to bee a good shooter'
+      fill_in 'Description', with: 'You will handle the security of the Death Star. You do not need to be a good shooter'
 
-      fill_in 'Company name', with: 'The Empire'
-      fill_in 'URL',          with: 'www.theempire.com'
-      fill_in 'Email',        with: 'hr@theempire.com'
+      within('#company-details') do
+        fill_in 'Name',   with: 'The Empire'
+        fill_in 'URL',    with: 'www.theempire.com'
+        fill_in 'E-mail', with: 'hr@theempire.com'
+      end
 
-      click_button 'Post job'
+      click_button 'Post vacancy'
     end
   end
 end
