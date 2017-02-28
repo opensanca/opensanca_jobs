01769f1eb6ac871c273efe5300bb1671076a045d
Carlos Ribeiro <mail@carlosribeiro.me>
2017-02-28 12:55:46 -0300

    Render flash message on the layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    {
+      success: 'alert-success',
+      error:   'alert-error',
+      alert:   'alert-block',
+      notice:  'alert-info'
+    }[flash_type.to_sym] || flash_type.to_s
+  end
+
 end
