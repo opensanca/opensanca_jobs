d1ccee311a5f3abfb0f1f1a4bb08f971ab5933e9
Carlos Ribeiro <mail@carlosribeiro.me>
2017-03-01 17:56:41 -0300

    Install and configure FactoryGirl

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require "spec_helper"
 require "rspec/rails"
 require "capybara/rails"
+require "support/factory_girl"
 
 ActiveRecord::Migration.maintain_test_schema!
 
