065377d2be00b52033ca960a3ebf0f945d481bf1
Carlos Ribeiro <mail@carlosribeiro.me>
2017-03-02 16:05:29 -0300

    Sanitize mostly of summernotes tags

---
+++ config/initializers/sanitize.rb
@@ -0,0 +1,5 @@
+Rails.application.config.action_view.sanitized_allowed_tags = %w(b strong span i em p h1 h2 h3 h4 h5 h6 blockquote pre
+font u table th td tr li ul a img)
+Rails.application.config.action_view.sanitized_allowed_attributes =  %w(face style href title src data-filename)
+
+Loofah::HTML5::WhiteList::ALLOWED_PROTOCOLS << 'data'
