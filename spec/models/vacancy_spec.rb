ae4fdaae43fd643bbc1c622ffcf37c57b569c670
Leonardo Ferreira <mail@leonardoferreira.com.br>
2017-03-04 16:01:21 -0300

    Validate Company URL and Company email on vacancy registration

--- spec/models/vacancy_spec.rb
+++ spec/models/vacancy_spec.rb
@@ -8,4 +8,18 @@ RSpec.describe Vacancy, type: :model do
   it { is_expected.to validate_presence_of(:company_name) }
   it { is_expected.to validate_presence_of(:company_url) }
   it { is_expected.to validate_presence_of(:company_email) }
+
+  context 'validate company email' do
+    it { is_expected.to allow_value('test@email.com').for(:company_email) }
+    it { is_expected.not_to allow_value('test').for(:company_email) }
+    it { is_expected.not_to allow_value('test@qwe').for(:company_email) }
+    it { is_expected.not_to allow_value('test.com').for(:company_email) }
+  end
+
+  context 'validate company url' do
+    it { is_expected.to allow_value('http://opensanca.com.br').for(:company_url) }
+    it { is_expected.to allow_value('www.opensanca.com.br').for(:company_url) }
+    it { is_expected.to allow_value('opensanca.com.br').for(:company_url) }
+    it { is_expected.not_to allow_value('invalid').for(:company_url) }
+  end
 end
