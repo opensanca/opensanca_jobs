65c3b35a0085b91b44baa00cea8c1a775d4574d7
Carlos Ribeiro <mail@carlosribeiro.me>
2017-03-04 20:50:37 -0300

    Add how to apply field to vacancy

--- spec/models/vacancy_spec.rb
+++ spec/models/vacancy_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Vacancy, type: :model do
   it { is_expected.to validate_presence_of(:job_title) }
   it { is_expected.to validate_presence_of(:location) }
   it { is_expected.to validate_presence_of(:description) }
+  it { is_expected.to validate_presence_of(:how_to_apply) }
 
   it { is_expected.to validate_presence_of(:company_name) }
   it { is_expected.to validate_presence_of(:company_url) }
