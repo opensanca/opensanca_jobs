7a56a305dd12d9984452d113ed79a1a73b9ef408
Carlos Ribeiro <mail@carlosribeiro.me>
2017-03-08 11:13:49 -0300

    Feature test for vacancies search

---
+++ spec/features/search_vacancies_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.feature "Search vacancies" do
+  scenario "keeping the search field empty" do
+    create_list(:vacancy, 3)
+
+    visit root_path
+
+    fill_in "Search", with: ""
+    find('.input-group-btn button').click
+
+    expect(page).to have_selector('section.vacancy-item', count: 3)
+  end
+
+  scenario "search for a specific vacancy" do
+    create_list(:vacancy, 2)
+    create(:vacancy, job_title: "Stormtrooper")
+
+    visit root_path
+
+    fill_in "Search", with: "Stormtrooper"
+    find('.input-group-btn button').click
+
+    expect(page).to have_selector('section.vacancy-item', count: 1)
+    expect(page).to have_content("Stormtrooper")
+  end
+end
