1849c978eda4024b5b0c58dc5b597f0d0008bb7b
Carlos Ribeiro <mail@carlosribeiro.me>
2017-03-13 19:28:40 -0300

    Sort the vacancies in the root page by the oldest to the newest

--- app/models/searchs/vacancy.rb
+++ app/models/searchs/vacancy.rb
@@ -4,35 +4,38 @@ class Searchs::Vacancy
   end
 
   def list(query = nil, lang = nil)
-    return @repository.all if query.blank?
+    return @repository.recent if query.blank?
 
     lang ||= I18n.locale == :'pt-BR' ? "portuguese" : "english"
 
     sql = %{
-      select
+      SELECT
         id,
         job_title,
         location,
         description,
         company_name,
+        created_at,
         document
-      from
-        (select
+      FROM
+        (SELECT
           id,
           job_title,
           location,
           description,
           company_name,
+          created_at,
           to_tsvector('#{lang}', location) || ' ' ||
           to_tsvector('#{lang}', company_name) || ' ' ||
           to_tsvector('#{lang}', job_title) || ' ' ||
-          to_tsvector('#{lang}', description) as document
-        from vacancies) vacancies
-      where
+          to_tsvector('#{lang}', description) AS document
+        FROM vacancies) vacancies
+      WHERE
         vacancies.document @@ to_tsquery('#{lang}', ?)
+      ORDER BY created_at DESC
     }
 
-    consult = query.split(' ').map { |e| "#{e}:*" }.join('&')
+    consult = query.split(' ').map { |entry| "#{entry}:*" }.join('&')
     @repository.find_by_sql([sql, consult])
   end
 
