71c7ce10976828a3d2467712e61951e752284d8f
Carlos Ribeiro <mail@carlosribeiro.me>
2017-03-24 21:01:16 -0300

    Add the default host for environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 end
