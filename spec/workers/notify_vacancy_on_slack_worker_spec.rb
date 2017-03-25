0c4c61694657c64dde9f797871e15c09ffad02f3
Carlos Ribeiro <mail@carlosribeiro.me>
2017-03-24 21:02:01 -0300

    Create the worker that will send the messages to slack

---
+++ spec/workers/notify_vacancy_on_slack_worker_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe NotifyVacancyOnSlackWorker, type: :worker do
+  describe '#perform' do
+    let(:vacancy_id) { create(:vacancy).id }
+
+    subject { described_class.new.perform(vacancy_id) }
+
+    it 'notifies the slack' do
+      expect_any_instance_of(SlackNotifier).to receive(:notify)
+
+      subject
+    end
+  end
+end
