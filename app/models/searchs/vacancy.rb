64696c1ee248a77f9ee6decc500201f50158d2cd
Carlos Ribeiro <mail@carlosribeiro.me>
2017-03-29 17:13:27 -0300

    Build the tsquery before the query

--- app/models/searchs/vacancy.rb
+++ app/models/searchs/vacancy.rb
@@ -6,6 +6,8 @@ class Searchs::Vacancy
   def list(query = nil)
     return @repository.recent if query.blank?
 
+    query = query.split(' ').map { |entry| "#{entry}:*" }.join(' & ')
+
     @repository.recent.where("tsv @@ to_tsquery('portuguese', :query)", query: query)
   end
 
