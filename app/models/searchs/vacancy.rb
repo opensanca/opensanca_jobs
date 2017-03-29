9a3f853e373cda9cf82a4045b05d5537ee5d8be8
Carlos Ribeiro <mail@carlosribeiro.me>
2017-03-29 16:29:29 -0300

    Remove unecessary manual query on Search::Vacancy

--- app/models/searchs/vacancy.rb
+++ app/models/searchs/vacancy.rb
@@ -6,23 +6,7 @@ class Searchs::Vacancy
   def list(query = nil)
     return @repository.recent if query.blank?
 
-    sql = %{
-      SELECT
-        id,
-        job_title,
-        location,
-        description,
-        company_name,
-        created_at,
-        slug
-      FROM vacancies
-      WHERE
-        vacancies.tsv @@ to_tsquery('portuguese', ?)
-      ORDER BY created_at DESC
-    }
-
-    consult = query.split(' ').map { |entry| "#{entry}:*" }.join('&')
-    @repository.find_by_sql([sql, consult])
+    @repository.where("tsv @@ to_tsquery('portuguese', :query)", query: query)
   end
 
   def self.list(query = nil)
