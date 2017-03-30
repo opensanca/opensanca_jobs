f23a530c14087042d174eac4d7bb93556b164b8b
Carlos Ribeiro <mail@carlosribeiro.me>
2017-03-30 14:15:52 -0300

    Remove not required RSpec before the describe block

--- spec/models/vacancy_spec.rb
+++ spec/models/vacancy_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Vacancy, type: :model do
+describe Vacancy, type: :model do
   it { is_expected.to validate_presence_of(:job_title) }
   it { is_expected.to validate_presence_of(:location) }
   it { is_expected.to validate_presence_of(:description) }
