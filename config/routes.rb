96f7b1619c865b85b8aa261743f9c1aca254dfed
Kadu Ribeiro <mail@carlosribeiro.me>
2018-03-06 09:58:47 -0300

    Restrict Sidekiq dashboard on production (#85)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USERNAME'])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD']))
+  end if Rails.env.production?
+
+  mount Sidekiq::Web => '/sidekiq'
+
   resources :passwords, controller: 'clearance/passwords', only: %i[create new]
   resource :session, controller: 'clearance/sessions', only: [:create]
 
@@ -15,8 +22,6 @@ Rails.application.routes.draw do
   get '/sign_in' => 'clearance/sessions#new', as: 'sign_in'
   delete '/sign_out' => 'clearance/sessions#destroy', as: 'sign_out'
   get '/sign_up' => 'clearance/users#new', as: 'sign_up'
-
-  mount Sidekiq::Web => '/sidekiq'
 
   resources :vacancies, except: %i[edit update destroy]
 
