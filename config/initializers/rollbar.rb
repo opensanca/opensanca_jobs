d2d5d0ddc25587c20267ec9fb8979014d186e35f
Carlos Ribeiro <mail@carlosribeiro.me>
2018-03-23 15:20:42 -0300

    Track the company in Rollbar

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -12,10 +12,12 @@ Rollbar.configure do |config|
   # By default, Rollbar will try to call the `current_user` controller method
   # to fetch the logged-in user object, and then call that object's `id`,
   # `username`, and `email` methods to fetch those properties. To customize:
-  # config.person_method = "my_current_user"
   # config.person_id_method = "my_id"
   # config.person_username_method = "my_username"
   # config.person_email_method = "my_email"
+
+  config.person_method          = 'current_company'
+  config.person_username_method = 'email'
 
   # If you want to attach custom data to all exception and message reports,
   # provide a lambda like the following. It should return a hash.
